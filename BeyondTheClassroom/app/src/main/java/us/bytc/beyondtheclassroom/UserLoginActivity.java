924d13ee6389db561193aaa2177ba9b7a4ed1adb
agoeden12 <andrewgoeden@gmail.com>
2017-11-17 22:51:48 -0500

    changed parent and teacher logins to one generic login

--- BeyondTheClassroom/app/src/main/java/us/bytc/beyondtheclassroom/UserLoginActivity.java
+++ BeyondTheClassroom/app/src/main/java/us/bytc/beyondtheclassroom/UserLoginActivity.java
@@ -3,11 +3,11 @@ package us.bytc.beyondtheclassroom;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TeacherLoginActivity extends AppCompatActivity {
+public class UserLoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teacher_login);
+        setContentView(R.layout.activity_user_login);
     }
 }
