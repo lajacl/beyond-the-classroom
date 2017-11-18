cab8c980e42c16dc8a46e5c6afc5e0691a86ef9f
agoeden12 <andrewgoeden@gmail.com>
2017-11-18 10:33:21 -0500

    adds an onClickListener to for the parent signup button to go to the parent signup activity.

--- BeyondTheClassroom/app/src/main/java/us/bytc/beyondtheclassroom/CreateAccountOptionsActivity.java
+++ BeyondTheClassroom/app/src/main/java/us/bytc/beyondtheclassroom/CreateAccountOptionsActivity.java
@@ -1,13 +1,30 @@
 package us.bytc.beyondtheclassroom;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAccountOptionsActivity extends AppCompatActivity {
+
+    private Button registerParentButton;
+
+    private Context mContext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account_options);
+
+        registerParentButton = findViewById(R.id.registerParentButtonOption);
+        registerParentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, ParentSignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
