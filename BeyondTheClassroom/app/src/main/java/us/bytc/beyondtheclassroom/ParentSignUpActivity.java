98ae2bbd3d1791931752bc9d416618aca0098472
agoeden12 <andrewgoeden@gmail.com>
2017-11-18 15:17:35 -0500

    created an add child page

--- BeyondTheClassroom/app/src/main/java/us/bytc/beyondtheclassroom/ParentSignUpActivity.java
+++ BeyondTheClassroom/app/src/main/java/us/bytc/beyondtheclassroom/ParentSignUpActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
