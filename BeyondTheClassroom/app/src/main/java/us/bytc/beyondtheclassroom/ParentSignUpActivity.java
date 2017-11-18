a5b6b13865207ec68d15cc29171b06566b5f486a
agoeden12 <andrewgoeden@gmail.com>
2017-11-18 14:09:37 -0500

    add id values to view in parentsignup

--- BeyondTheClassroom/app/src/main/java/us/bytc/beyondtheclassroom/ParentSignUpActivity.java
+++ BeyondTheClassroom/app/src/main/java/us/bytc/beyondtheclassroom/ParentSignUpActivity.java
@@ -1,13 +1,28 @@
 package us.bytc.beyondtheclassroom;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ParentSignUpActivity extends AppCompatActivity {
+
+    private Context mContext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_sign_up);
+
+        Button registerParentButton = findViewById(R.id.createParentAccountButton);
+        registerParentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, addChildActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ BeyondTheClassroom/app/src/main/java/us/bytc/beyondtheclassroom/addChildActivity.java
@@ -0,0 +1,13 @@
+package us.bytc.beyondtheclassroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addChildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_child);
+    }
+}
