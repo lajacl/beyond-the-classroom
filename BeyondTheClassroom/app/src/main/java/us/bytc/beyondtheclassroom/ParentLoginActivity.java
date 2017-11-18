356a00b7e31767a9cc04b67ffd627821b8fa506b
agoeden12 <andrewgoeden@gmail.com>
2017-11-17 22:44:40 -0500

    added basic login and sign up activities

---
+++ BeyondTheClassroom/app/src/main/java/us/bytc/beyondtheclassroom/ParentLoginActivity.java
@@ -0,0 +1,13 @@
+package us.bytc.beyondtheclassroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParentLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent_login);
+    }
+}
