3360d96dded046b4e0273c7a25c7e7b1bf2e19bd
agoeden12 <andrewgoeden@gmail.com>
2017-11-18 10:21:48 -0500

    adds an onClickListener to for the create activity button.

--- BeyondTheClassroom/app/src/main/java/us/bytc/beyondtheclassroom/UserLoginActivity.java
+++ BeyondTheClassroom/app/src/main/java/us/bytc/beyondtheclassroom/UserLoginActivity.java
@@ -1,13 +1,30 @@
 package us.bytc.beyondtheclassroom;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserLoginActivity extends AppCompatActivity {
+
+    private Button createAccountButton;
+
+    private Context mContext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        createAccountButton = findViewById(R.id.createNewUserAccountButton);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, CreateAccountOptionsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
