1f195c3a3e6bec41bac3e2216b1732dcf8674ffc
LaVette <lavette.clifton@gmail.com>
2017-11-18 05:28:41 -0500

    created containers for login input views, login button listener, & signIn method

--- BeyondTheClassroom/app/src/main/java/us/bytc/beyondtheclassroom/UserLoginActivity.java
+++ BeyondTheClassroom/app/src/main/java/us/bytc/beyondtheclassroom/UserLoginActivity.java
@@ -2,12 +2,37 @@ package us.bytc.beyondtheclassroom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UserLoginActivity extends AppCompatActivity {
+    EditText usernameEditText;
+    EditText passwordEditText;
+
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        usernameEditText = (EditText) findViewById(R.id.editTextUsername);
+        passwordEditText = (EditText) findViewById(R.id.editTextPassword);
+
+        loginBtn = (Button) findViewById(R.id.btnLogin);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    //gets user input from view
+    private void signIn() {
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
     }
 }
